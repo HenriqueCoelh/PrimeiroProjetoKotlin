fff3781c056267796cfc2c594d65379197e5468b
Henrique Coelho <coelho.heriquesantos@hotmail.com>
2021-10-22 12:36:48 -0300

    Pasta PrimeiroProjetoKotlin foi adicionado

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println("<???> Opa, eae")
+    println("<Rogério> De boas?")
+    println("<???> Suaves")
+    println("<Rogério> Qual seu nome?")
+    println("<???> Meu nome é Henrique")
+    println("<Rogério> Opa, prazer em conhece-lo Henrique")
+    println("<???> O prazer é todo meu!")
+
+}
